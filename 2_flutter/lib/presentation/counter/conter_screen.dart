c5e060c062035f95d97e0eae401b20931d3b8f57
Pablo Marquez <pamarquez90@gmail.com>
2025-05-03 20:09:19 -0300

    made theme responsive on counter screen

--- 2_flutter/lib/presentation/counter/conter_screen.dart
+++ 2_flutter/lib/presentation/counter/conter_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutterbasics/application/theme_service.dart';
+import 'package:provider/provider.dart';
 
 class CounterScreen extends StatefulWidget {
   const CounterScreen({super.key});
@@ -11,42 +13,47 @@ class _CounterScreenState extends State<CounterScreen> {
   int _counter = 0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.primaryContainer,
-      appBar: AppBar(title: const Text('Counter App')),
-      body: Center(
-        child: Text(
-          _counter.toString(),
-          style: TextStyle(fontSize: 70, fontWeight: FontWeight.bold, color: _counter > 0 ? Theme.of(context).primaryColor : Colors.black),
+    return Consumer<ThemeService>(builder: ((context, themeService, child) {
+      Color primaryColor = themeService.isDarkModeOn ? Theme.of(context).colorScheme.primary : Theme.of(context).colorScheme.onPrimary;
+      Color secondaryColor = themeService.isDarkModeOn ? Theme.of(context).colorScheme.secondary : Theme.of(context).colorScheme.onSecondary;
+
+      return Scaffold(
+        backgroundColor: themeService.isDarkModeOn ? Theme.of(context).colorScheme.primaryContainer : Theme.of(context).colorScheme.onPrimaryContainer,
+        appBar: AppBar(title: const Text('Counter App')),
+        body: Center(
+          child: Text(
+            _counter.toString(),
+            style: TextStyle(fontSize: 70, fontWeight: FontWeight.bold, color: _counter > 0 ? primaryColor : secondaryColor),
+          ),
         ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            FloatingActionButton(
-                heroTag: 'btn1',
-                onPressed: () {
-                  setState(() {
-                    _counter = _counter - 1;
-                  });
-                },
-                backgroundColor: Theme.of(context).colorScheme.secondary,
-                child: const Icon(Icons.remove)),
-            FloatingActionButton(
-                heroTag: 'btn2',
-                onPressed: () {
-                  setState(() {
-                    _counter = _counter + 1;
-                  });
-                },
-                backgroundColor: Theme.of(context).colorScheme.primary,
-                child: const Icon(Icons.add)),
-          ],
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              FloatingActionButton(
+                  heroTag: 'btn1',
+                  onPressed: () {
+                    setState(() {
+                      _counter = _counter - 1;
+                    });
+                  },
+                  backgroundColor: secondaryColor,
+                  child: Icon(Icons.remove, color: themeService.isDarkModeOn ? Theme.of(context).colorScheme.onPrimary : Theme.of(context).colorScheme.primary)),
+              FloatingActionButton(
+                  heroTag: 'btn2',
+                  onPressed: () {
+                    setState(() {
+                      _counter = _counter + 1;
+                    });
+                  },
+                  backgroundColor: themeService.isDarkModeOn ? Theme.of(context).colorScheme.primary : Theme.of(context).colorScheme.onPrimary,
+                  child: Icon(Icons.add, color: themeService.isDarkModeOn ? Theme.of(context).colorScheme.onPrimary : Theme.of(context).colorScheme.primary)),
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    }));
   }
 }
